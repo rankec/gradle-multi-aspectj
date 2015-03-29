8708b0ebef09fb8737f239c241ceac148d992b5f
Rankec <rankec2221@gmail.com>
2015-03-29 21:20:58 +0200

    full project

---
+++ src/main/java/com/kodcu/app/devices/NoteBook.java
@@ -0,0 +1,24 @@
+package com.kodcu.app.devices;
+
+import com.kodcu.app.data.Brand;
+
+/**
+ * 
+ * @author HAKAN
+ *
+ */
+public class NoteBook {
+
+	public NoteBook() {
+		super();
+	}
+	 
+	public NoteBook(String type) {
+		super(type);
+	}
+	
+	public NoteBook(String type,Brand brand) {
+		super(type,brand);
+	}
+	
+}
